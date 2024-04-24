73d34463e36725a32b7faf8a83b49815bc0bc3ba
Pablo1864 <pablonpenaloza@sanluis.edu.ar>
2024-04-23 21:57:59 -0300

    geo

--- app/src/main/java/com/app/examenfinal/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/app/examenfinal/ui/gallery/GalleryFragment.java
@@ -1,5 +1,6 @@
 package com.app.examenfinal.ui.gallery;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.app.examenfinal.databinding.FragmentGalleryBinding;
@@ -25,7 +27,13 @@ public class GalleryFragment extends Fragment {
         View root = binding.getRoot();
 
         final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        galleryViewModel.getLocation().observe(getViewLifecycleOwner(), new Observer<Location>() {
+            @Override
+            public void onChanged(Location location) {
+               textView.setText("Latitud"+ location.getLatitude()+"Longitud"+location.getLongitude());
+            }
+        });
+        galleryViewModel.obtenerUltimaUbicacion();
         return root;
     }
 
--- app/src/main/java/com/app/examenfinal/ui/gallery/GalleryViewModel.java
+++ app/src/main/java/com/app/examenfinal/ui/gallery/GalleryViewModel.java
@@ -1,19 +1,98 @@
 package com.app.examenfinal.ui.gallery;
 
+import android.Manifest;
+import android.app.Application;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+import com.google.android.gms.location.*;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
-    private final MutableLiveData<String> mText;
+public class GalleryViewModel extends AndroidViewModel {
 
-    public GalleryViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+    private MutableLiveData<Location> mLocation;
+    private FusedLocationProviderClient fused;
+
+    public GalleryViewModel(@NonNull Application application) {
+        super(application);
+        fused = LocationServices.getFusedLocationProviderClient(getApplication());
     }
 
-    public LiveData<String> getText() {
-        return mText;
+
+    public LiveData<Location> getLocation() {
+        if (mLocation == null) {
+
+            this.mLocation = new MutableLiveData<>();
+        }
+        return mLocation;
     }
+
+    public void obtenerUltimaUbicacion() {
+
+        Task<Location> tarea;
+        if (ActivityCompat.checkSelfPermission(getApplication(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplication(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        tarea = fused.getLastLocation();
+        tarea.addOnSuccessListener(getApplication().getMainExecutor(), new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+
+                    mLocation.postValue(location);
+                }
+            }
+        });
+    }
+
+
+    public void lecturaPermanente() {
+
+        LocationRequest request = new LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 5000).build();
+
+        LocationCallback callback = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                Location location = locationResult.getLastLocation();
+                mLocation.postValue(location);
+            }
+
+        };
+
+        if (ActivityCompat.checkSelfPermission(getApplication(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplication(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fused.requestLocationUpdates(request, callback, null);
+    }
+
+
 }
